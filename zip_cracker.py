fbef42e6a0b8ad60189afedf7b1a2cfb2391888c
José Manuel Ortega <jmoc25@gmail.com>
2021-08-26 15:39:11 +0200

    Add files via upload

---
+++ zip_cracker.py
@@ -0,0 +1,15 @@
+import zipfile
+import time
+encrypted_filename= "secret_file.zip"
+zFile = zipfile.ZipFile(encrypted_filename, "r")
+
+passFile = open("passwords_zip.txt", "r")
+for line in passFile.readlines():
+    test_password = line.strip("\n").encode('utf-8')
+    try:
+        print(test_password)
+        zFile.extractall(pwd=test_password)
+        print("Match found")
+        break
+    except Exception as err:
+        pass
